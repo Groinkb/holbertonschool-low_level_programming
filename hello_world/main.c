40046c29bdbf377aba8053c2587f1d2f76f6464e
Groinkb <benjamin@mossu.ch>
2023-06-15 08:05:40 -0700

    CommentaireExo0enC

--- hello_world/main.c
+++ hello_world/main.c
@@ -1,12 +1,18 @@
 #!/bin/bash
 #include <stdio.h>
-
+/* c'est une directive au compilateur dont le but est de réaliser l'inclusion mentionnée . */
    /**
     * main - Entry point
+    /* Chaque programme C possède une fonction principale qui doit être nommée main. La fonction main sert de point de départ pour l’exécution du programme. Elle contrôle généralement l'exécution du programme en dirigeant les appels à d'autres fonctions du programme. */
+
+
+
+    // J'essaye un commentaire avec deux slashs pour voir
     *
     * Return: Always 0 (Success)
     */
 int main(void)
+// where the execution of program begins
 {
   return (0);
 }
