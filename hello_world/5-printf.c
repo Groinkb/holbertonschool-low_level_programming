1e07df58db0191182a38a39257d62e28ebfca85c
Groinkb <benjamin@mossu.ch>
2023-06-16 05:43:02 -0700

    Exo5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+  printf("hello, world\n") ;
+
+  return 0 ;
+
+}
+/*Include c'est pour *The C programming language provides many standard library functions for file input and output. These functions make up the bulk of the C standard library header <stdio.h>.*/
+
