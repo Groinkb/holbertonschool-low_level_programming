17bd0e776300da1c720eeaba01c5565bbb2b9e1a
Groinkb <benjamin@mossu.ch>
2023-06-19 04:01:05 -0700

    Exo2IFCV2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,6 @@
-#include <stdio.h>                                                                                                                              
-int main (void)
+#include <stdio.h>                                                                                                                             int main(void) //fonction void principale du programme
 {
 char c;
-
 for (c = 'a'; c <= 'z'; c++)
 putchar(c);
 putchar('\n');
