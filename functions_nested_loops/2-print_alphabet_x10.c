38d6f575249121409bee12a31470f6e863cf14e7
Groinkb <benjamin@mossu.ch>
2023-06-20 01:00:05 -0700

    Exo2V3FC

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,18 @@
 #include "main.h"
+
+
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lower
- * Return : Always return 0
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
  */
 void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
 
+
 	i = 0;
+
 
 	while (i < 10)
 		{
@@ -17,8 +21,3 @@ void print_alphabet_x10(void)
 				{
 					_putchar(ch);
 					ch++;
-				}
-			_putchar('\n');
-			i++;
-		}
-}
