0484e42c2a20ce18e11e167e4f50b1419b113806
Groinkb <benjamin@mossu.ch>
2023-06-23 02:14:58 -0700

    Exo0&main

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,16 @@
-#include <main.h>
+#include "main.h"
 
-/* main - Write a function that checks for uppercase character.
- * Returns 1 if c is uppercase
-*Returns 0 otherwise */
+/**
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
 
-int _isupper(int c);
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return (1)
+		return (1);
 	else
-		return (0)
+		return (0);
 }
