c1af6090c97a9f517c07ce4d5f6411897693e513
Groinkb <benjamin@mossu.ch>
2023-06-27 00:17:47 -0700

    Exo0StringC

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main - check the code 
- *
- * Return: Always 0.
- */
-int main(void)
+* reset_to_98 - an function that takes a pointer to an int
+* updates the value it points to to 98.
+* @n:pointer that contain a value A pointer is a variable
+*/
+void reset_to_98(int *n)
 {
-	void updateValue(int* ptr) {
-     	*ptr = 98;
+	*n = 98;
 }
