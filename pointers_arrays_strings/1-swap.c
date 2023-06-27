b87835d00b12c44f31be92993c316df9ae7e4ef9
Groinkb <benjamin@mossu.ch>
2023-06-27 03:17:04 -0700

    Exo1CString

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- *swap_int - swaps the values of two integer
- * @a:value that will swap
- * @b:value that will swap
- */
-void swap_int(int *a, int *b)
+ * swap_int - a function that swaps the values of two integers.
+ * @a: a int that become swap
+ * @b: a int that become swap
+ * Add the two values
+ * Subtract the second value from the sum
+ * Subtract the first value from the sum
+*/
 {
-    *a = *a + *b;
-    *b = *a - *b;
-    *a = *a - *b;
+*a = *a + *b;
+*b = *a - *b;
+*a = *a - *b;
 }
-
