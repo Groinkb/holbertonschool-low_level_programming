81af3889bc5bf4322c9ba9b2869e17948390e2e3
Groinkb <benjamin@mossu.ch>
2023-06-27 06:12:24 -0700

    Exo2Cstring

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,8 @@
  * main - check the code
  * a function that returns the length of a string.
  * Return: Always 0.
- *
+ * * _strlen - a function that returns the length of a string
+ * @s: a function who's been count
  */
 int _strlen(char *s)
 {
