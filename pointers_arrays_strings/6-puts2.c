dc77458169c72651b35c2d9fa1c3de81fb7902f1
Groinkb <benjamin@mossu.ch>
2023-06-28 13:47:37 -0700

    Exo6CSTRING

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string.
+ * @str: The string to print.
+ */
+void puts2(char *str)
+{
+	if (str == NULL)
+		return;
+
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+
+	_putchar('\n');
+}
