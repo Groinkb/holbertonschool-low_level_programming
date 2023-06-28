6a6e44c3ec96013af09483d1b20821410a0105e6
Groinkb <benjamin@mossu.ch>
2023-06-28 01:55:27 -0700

    Exo2Cstring

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen - a function that returns the length of a string
- * @s: a pointer to the string
+ * _strlen - Retourne la longueur d'une chaîne de caractères.
+ * @s: Pointeur vers la chaîne de caractères.
  *
- * Return: The length of the string
+ * Return: La longueur de la chaîne de caractères.
  */
-
 int _strlen(char *s)
-
-char *ptr = s;
 {
+	char *ptr = s;
+
 	while (*ptr)
 	{
 		ptr++;
-		}
+	}
+
 	return (int)(ptr - s);
 }
