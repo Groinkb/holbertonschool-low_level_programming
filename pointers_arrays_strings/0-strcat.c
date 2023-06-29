e9125adae4f3347de961cb94e487f36069b3d290
Groinkb <benjamin@mossu.ch>
2023-06-29 06:35:25 -0700

    Exo0P2CString

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Destination string
+ * @src: Source string
+ *
+ * Return: Pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+
+	*ptr = '\0';
+
+	return (dest);
+}
