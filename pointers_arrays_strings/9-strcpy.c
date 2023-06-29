dd61b8e85cf220ce01b2ed2d90407644c88cf465
Groinkb <benjamin@mossu.ch>
2023-06-29 02:50:24 -0700

    Exo9Cstring

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,24 @@
+#include "main.h"
+
 /**
- * _strcpy - Copies the string pointed.
- * @dest: Pointer to the destination buffer.
- * @src: Pointer to the source string.
+ * _strcpy - Copies the string pointed to by src
+ *
+ * @dest: Destination buffer.
+ * @src: Source string.
  *
  * Return: Pointer to dest.
  */
 char *_strcpy(char *dest, char *src)
 {
-	char *dest_start = dest;
+	int index = 0;
 
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
+	while (src[index])
+		{
+		dest[index] = src[index];
+		index++;
 	}
 
-	*dest = '\0';
+	dest[index] = '\0';
 
-	return (dest_star);
+	return (dest);
 }
