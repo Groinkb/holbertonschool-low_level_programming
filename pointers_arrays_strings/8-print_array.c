8e0d980e8e8300c4d3572fbeb3ccd208bb16f761
Groinkb <benjamin@mossu.ch>
2023-06-29 02:13:51 -0700

    Exo8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints n elements of an array ofnaintegrer
- * @a: Array to be printed.
- * @n: Number of elements to print.
+ * print_array - Prints n elements of an array.
+ * @a: The array of integers.
+ * @n: The number of elements to be printed.
  */
 void print_array(int *a, int n)
 {
-	int i;
+	int index;
 
-	if (n <= 0 || a == NULL)
-		return;
+	for (index = 0; index < n; index++)
+		{
+		printf("%d", a[index]);
 
-	printf("%d", a[0]);
+		if (index == n - 1)
+			continue;
 
-	for (i = 1; i < n; i++)
-	{
-		printf(", %d", a[i]);
+		printf(", ");
 	}
 
 	printf("\n");
