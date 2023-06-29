9defdbee6ba902664418313ec80c8f971478d9f5
Groinkb <benjamin@mossu.ch>
2023-06-29 06:51:11 -0700

    Exo1P2T1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings
+ * @dest: Pointer to the destination string
+ * @src: Pointer to the source string
+ * @n: Number of bytes to be concatenated
+ *
+ * Return: Pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i;
+
+
+	while (dest[dest_len] != '\0')
+		dest_len++;
+
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len++] = src[i];
+
+	dest[dest_len] = '\0';
+
+	return (dest);
+}
