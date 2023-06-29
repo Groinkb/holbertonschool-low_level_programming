3703ece29aece55a810fece23e0371090edad720
Groinkb <benjamin@mossu.ch>
2023-06-29 07:01:39 -0700

    Exo2P2Cstring

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Number of characters to copy
+ *
+ * Return: Pointer to the destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *original_dest = dest;
+
+	while (*src && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	while (n > 0)
+	{
+		*dest = '\0';
+		dest++;
+		n--;
+	}
+
+	return (original_dest);
+}
