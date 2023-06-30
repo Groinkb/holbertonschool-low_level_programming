65e3282a3c95fa64827a89f5526d3fce115663ca
Groinkb <benjamin@mossu.ch>
2023-06-30 12:04:55 -0700

    Exo4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverse the content of an array of integers
+ * @a: Pointer to the array
+ * @n: Number of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int tempVar, start, end;
+
+	for (start = 0, end = n - 1; start < end; start++, end--)
+	{
+		tempVar = a[start];
+		a[start] = a[end];
+		a[end] = tempVar
+	}
+}
