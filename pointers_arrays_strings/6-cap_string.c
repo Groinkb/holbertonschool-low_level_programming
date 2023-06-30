5286202ee321973fa8af49057c82c0fac6047fe2
Groinkb <benjamin@mossu.ch>
2023-06-30 16:30:54 -0700

    exo6STRING

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * cap_string - Met en majuscule tous les mots dans une chaîne de caractères.
- * @str: Chaîne de caractères à mettre en majuscule.
+ * cap_string - Capitalize all words in a string.
+ * @str: String to capitalize.
  *
- * Return: Pointeur vers la chaîne de caractères en majuscule.
+ * Return: Pointer to the capitalized string.
  */
 char *cap_string(char *str)
 {
@@ -15,14 +15,24 @@ char *cap_string(char *str)
 
 	while (str[count] != '\0')
 	{
-		if (str[count] == ' ' || str[count] == '\t' || str[count] == '\n' ||
-		    str[count] == ',' || str[count] == ';' || str[count] == '.' ||
-		    str[count] == '!' || str[count] == '?' || str[count] == '"' ||
-		    str[count] == '(' || str[count] == ')' || str[count] == '{' ||
-		    str[count] == '}')
+		switch (str[count])
 		{
-			if (str[count + 1] >= 'a' && str[count + 1] <= 'z')
-				str[count + 1] -= 32;
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				if (str[count + 1] >= 'a' && str[count + 1] <= 'z')
+					str[count + 1] -= 32;
+				break;
 		}
 		count++;
 	}
