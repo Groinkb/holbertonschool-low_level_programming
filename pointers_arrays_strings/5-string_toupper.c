c19dee3f60c69eaa70c583d967dd88ef3f67763e
Groinkb <benjamin@mossu.ch>
2023-06-30 12:11:05 -0700

    Exo5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * rev_string - Reverses a string.
+ * @s: The string to be reversed.
+ */
+void rev_string(char *s)
+{
+	int length, index;
+	char *begin, *end, character;
+
+	length = 0;
+	while (s[length] != '\0')
+		length++;
+
+	begin = s;
+	end = s + length - 1;
+
+	for (index = 0; index < length / 2; index++)
+	{
+		character = *end;
+		*end = *begin;
+		*begin = character;
+
+		begin++;
+		end--;
+	}
+}
