d9fe8a5dcb88eccf274be3e315a86ad4eb012adb
Groinkb <benjamin@mossu.ch>
2023-07-02 09:49:25 -0700

    exo3P3c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - Calculates the length of a prefix substring.
+ * @s: Pointer to the string to search.
+ * @accept: Pointer to the string containing the accepted characters.
+ *
+ * Return: Length of the initial segment.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int length = 0;
+	unsigned int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				length++;
+				break;
+			}
+		}
+
+		if (!accept[i])
+			break;
+
+		s++;
+	}
+
+	return (lenght)
+}
