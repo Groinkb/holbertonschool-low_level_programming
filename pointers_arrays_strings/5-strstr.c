53d24af5ab892d2442c879f19fc7829cde9a0e50
Groinkb <benjamin@mossu.ch>
2023-07-02 11:54:43 -0700

    exo5p3C

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring.
+ * @haystack: Pointer to the string to search.
+ * @needle: Pointer to the substring to locate.
+ *
+ * Return: Pointer to the beginning of the located substring,
+ *         or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n && *h && (*h == *n))
+		{
+			h++;
+			n++;
+		}
+
+		if (!(*n))
+			return (haystack);
+
+		haystack++;
+	}
+
+	return (NULL);
+}
