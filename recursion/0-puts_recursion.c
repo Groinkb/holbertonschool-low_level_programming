763673093bbf3ae99fbde24a1845fbd9d77ad5d6
Groinkb <benjamin@mossu.ch>
2023-07-04 00:14:46 -0700

    Exo0COmit

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,1 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+
+void _puts_recursion(char *s) {
+    if (*s == '\0') {
+        printf("\n");
+    } else {
+        printf("%c", *s);
+        _puts_recursion(s + 1);
+    }
