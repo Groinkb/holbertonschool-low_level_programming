a89b109c6a350372e971fc7cec245649c549a74b
Groinkb <you@example.com>
2023-07-04 16:17:13 +0200

    TASK0AFTER10DAYSONIT

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts_recursion - prints a string, followed by a new line.
@@ -7,15 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
 
-    _putchar(*s);
-    _puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
-
-
-
