421c98af487148c38e6aed7609f6706c225028e1
Groinkb <benjamin@mossu.ch>
2023-07-05 18:34:18 +0200

    Exo3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -8,16 +8,11 @@
  */
 int factorial(int n)
 {
-	int result = 1;
+	if (n == 0)
+		return (1);
 
 	if (n < 0)
-		return -1;
+		return (-1);
 
-	while (n > 0)
-	{
-		result *= n;
-		n--;
-	}
-
-	return result;
+	return (n * factorial(n - 1));
 }
