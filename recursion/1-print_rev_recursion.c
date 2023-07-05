8b615088b228ca0769913b6a8ad6937dbcfa1f46
Groinkb <benjamin@mossu.ch>
2023-07-05 09:47:10 +0200

    test

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: a string
+ * _print_rev_recursion - imprime une chaîne de caractères en ordre inverse
+ * @s: la chaîne de caractères à imprimer
  */
-
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
+	if (*s == '\0')
 		return;
 
 	_print_rev_recursion(s + 1);
