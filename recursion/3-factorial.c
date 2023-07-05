1a1b53081fbcddb09cbda6ae02cec29603cd30bc
Groinkb <benjamin@mossu.ch>
2023-07-05 10:08:20 +0200

    CorrectionExo3C

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -9,13 +9,15 @@
 int factorial(int n)
 {
 	int result = 1;
-	int i;
 
 	if (n < 0)
-		return (-1);
+		return -1;
 
-	for (i = 1; i <= n; i++)
-		result *= i;
+	while (n > 0)
+	{
+		result *= n;
+		n--;
+	}
 
 	return result;
 }
