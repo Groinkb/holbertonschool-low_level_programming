4da89cae1a6bada18698c810004ecc8c2cf7de0d
Groinkb <you@example.com>
2023-07-06 10:19:16 +0200

    chang1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
 /**
+<<<<<<< HEAD
  * _print_rev_recursion - imprime une chaîne de caractères en ordre inverse
  * @s: la chaîne de caractères à imprimer
+=======
+ * _puts_rev- prints a string, followed by a new line.
+ * @s: a string
+>>>>>>> 32df76c (chang1)
  */
 void _print_rev_recursion(char *s)
 {
