dde31824ccb56075a0257c507ef9ed1e05bd5810
Groinkb <you@example.com>
2023-07-06 14:46:11 +0200

    Exo5101

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -4,9 +4,6 @@
  * @n: number is squared and compared against base
  * @base: base number to check
  * Return: natural square root of number base
- * * _sqrt_recursion - return the natural square root of a number n.
- * @n: number to check for square roots.
- * Return: the natural square root of number n
  */
 int checker(int n, int base)
 {
@@ -16,6 +13,13 @@ int checker(int n, int base)
 		return (-1);
 	return (checker(n + 1, base));
 }
+/**
+ * _sqrt_recursion - return the natural square root of a number n.
+ * @n: number to check for square roots.
+ * Return: the natural square root of number n
+ * int number without coma.
+ * if = condition
+ */
 int _sqrt_recursion(int n)
 {
 	return (checker(1, n));
