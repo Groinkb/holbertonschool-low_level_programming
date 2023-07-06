5d60aad4a88e6a49e7c8e9abafcadd26cb7385dc
Groinkb <you@example.com>
2023-07-06 14:49:49 +0200

    Exo5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
+
 /**
  * checker - checks the input number from n to the base
  * @n: number is squared and compared against base
  * @base: base number to check
- * Return: natural square root of number base
+ * Return: natural square root of the base number if found, -1 otherwise.
  */
 int checker(int n, int base)
 {
-	if (n * n == base)
-		return (n);
-	if (n * n > base)
-		return (-1);
-	return (checker(n + 1, base));
+    if (n * n == base)
+        return n;
+    if (n * n > base)
+        return (-1);
+    return checker(n + 1, base);
 }
+
 /**
  * _sqrt_recursion - return the natural square root of a number n.
  * @n: number to check for square roots.
- * Return: the natural square root of number n
- * int number without coma.
- * if = condition
+ * Return: the natural square root of number n.
+ * int : number without coma
+ * if : condition
  */
 int _sqrt_recursion(int n)
 {
-	return (checker(1, n));
+    return checker(1, n);
 }
