2ed28266eb0ea228813e9bf6fc218f8671024416
Groinkb <benjamin@mossu.ch>
2023-07-07 11:24:38 +0200

    README

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  * main - programme that prints its name
  * @argc: unused parameter
  * @argv: used to print the name of the programme
- * Return: 0 always
+ * Return: 0 always.
  */
 
 int main(int argc, char **argv)
