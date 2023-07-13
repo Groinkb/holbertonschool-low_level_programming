8bc38f5204f13d34f11cda24debae9f989b0b885
Groinkb <you@example.com>
2023-07-13 14:34:24 +0200

    Malloc0V2

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,19 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 #include <stdlib.h>
 #include <stdio.h>
 
 void *malloc_checked(unsigned int b) {
     void *ptr = malloc(b);
     if (ptr == NULL) {
-        fprintf(stderr, "Failed to allocate memory\n");
+        fprintf(stderr, "[stderr]: Failed to allocate memory\n");
         exit(98);
     }
     return ptr;
+}
+
+int main() {
+    void *ptr = malloc_checked(10);
+    printf("msg - [Got]\n");
+    printf("%d\n", *((int*)ptr));  // Just an example usage
+    free(ptr);
+    return 0;
 }
