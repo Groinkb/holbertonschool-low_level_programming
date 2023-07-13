28f7416d73fa6e7b75a15fee59bdcb932be15349
Groinkb <you@example.com>
2023-07-13 14:33:03 +0200

    Malloc0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,2 +1,15 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void *malloc_checked(unsigned int b) {
+    void *ptr = malloc(b);
+    if (ptr == NULL) {
+        fprintf(stderr, "Failed to allocate memory\n");
+        exit(98);
+    }
+    return ptr;
+}
