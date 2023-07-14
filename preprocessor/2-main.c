ea043bfd8ed6fb76f308f678e7771ef9e41040cd
Groinkb <benjamin@mossu.ch>
2023-07-14 15:58:19 +0200

    Exo2Main

---
+++ preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the name of the file being compiled
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return 0;
+}
