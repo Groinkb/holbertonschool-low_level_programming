aed80f3121b00374d7393ce11200971604d521cb
Groinkb <benjamin@mossu.ch>
2023-07-14 15:13:37 +0200

    mainmalloc

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,32 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * malloc_checked - allocate memory with malloc
- * @b: unsigned int type
- * Return: return pointer
+ * malloc_checked - Allocate memory using malloc
+ * @b: Size of memory to allocate
+ *
+ * Return: Pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
-    void *ptr;
+    void *ptr = malloc(b);
 
-    ptr = malloc(b);
     if (ptr == NULL)
-    {
         exit(98);
-    }
+
     return (ptr);
-}
-
-int main(void)
-{
-    unsigned int size = 10;
-    int *array = malloc_checked(size * sizeof(int));
-
-    /* Utilisez le tableau alloué ici... */
-
-    free(array); /* N'oubliez pas de libérer la mémoire allouée */
-
-    return (0);
 }
