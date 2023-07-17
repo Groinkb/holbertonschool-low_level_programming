28d2f4869d8e271b84aaa9b7ab9d881616c1e3d6
Groinkb <benjamin@mossu.ch>
2023-07-17 12:03:15 +0200

    dogchange

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -5,7 +5,7 @@
  * struct dog - structure
  * @name: name of the dog
  * @age: age of the dog
- * @owner: owner of the dog
+ * @owner: owner of the dogo
  *
  * Description: dog with its name, its age, and its owner's name.
  */
