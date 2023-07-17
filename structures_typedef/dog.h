707423820704ce32b3db6cfb21e207c173bf8314
Groinkb <benjamin@mossu.ch>
2023-07-17 12:08:05 +0200

    nosspace

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -12,9 +12,9 @@
 
 typedef struct dog
 {
-    char *owner;
-    float age;
-    char *name;
+char *owner;    
+float age;
+char *name;
 } dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
