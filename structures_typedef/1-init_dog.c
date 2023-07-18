ca2f23f23a8cf8fad37033a81db263f14acc0ff7
Groinkb <you@example.com>
2023-07-18 12:37:44 +0200

    DofFunction

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,23 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include "dog.h"
-/*
-init_dog - a function that initialize a variable of type struct dog
+
+/**
+ * init_dog - a function that initialize a variable of type struct dog
  *
  * @d: structure of dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  *
-*/
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		strcpy(d->name, name);
+		d->name = name;
 		d->age = age;
-		strcpy(d->owner, owner);
-
- }
+		d->owner = owner;
+	}
 }
