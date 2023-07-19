35a7ff1c1fd64c777811afd84f1018904cea032d
Groinkb <benjamin@mossu.ch>
2023-07-19 23:32:55 +0200

    Func4dOG

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - Create a new dog_t structure
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Return: Pointer to a new dog_t structure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog = malloc(sizeof(dog_t));
+
+	if (newDog == NULL)
+		return (NULL);
+
+	newDog->name = malloc(strlen(name) + 1);
+	if (newDog->name == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+
+	newDog->owner = malloc(strlen(owner) + 1);
+	if (newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog);
+		return (NULL);
+	}
+
+	strcpy(newDog->name, name);
+	strcpy(newDog->owner, owner);
+
+	newDog->age = age;
+
+	return (newDog);
+}
