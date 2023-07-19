ea46b05c3f4c2524c3cf0ab4180f307e40671d92
Groinkb <benjamin@mossu.ch>
2023-07-19 23:29:30 +0200

    dog-t

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,6 +17,8 @@ typedef struct dog
 	char *name;
 } dog_t;
 
+typedef struct dog dog_yt;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
