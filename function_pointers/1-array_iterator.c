24f32d517aceb9b5a7425d831e57ea7f109922da
Groinkb <you@example.com>
2023-07-21 16:55:41 +0200

    ModificationBoucleFor

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -4,16 +4,17 @@
  * array_iterator - Executes a function given as a parameter
  *                  on each element of an array.
  *
- * @array: Array of  the integers.
+ * @array: Array of integers.
  * @size: Size of the array.
  * @action: Pointer to the function.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t i;
+
 	if (!array || !action)
 		return;
 
-	size_t i;
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
