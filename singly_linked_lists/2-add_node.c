e0beff9048b100567d720ec132d0234565054856
Groinkb <benjamin@mossu.ch>
2023-08-02 20:17:35 +0200

    New node as 90

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -15,14 +15,18 @@ list_t *add_node(list_t **head, const char *str)
 	if (str == NULL)
 		return (NULL);
 
-	size_t len = strlen(str);
 	list_t *new_node = malloc(sizeof(list_t));
-
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = len;
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
 	new_node->next = *head;
 
 	*head = new_node;
