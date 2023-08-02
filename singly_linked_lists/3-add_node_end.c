6a5bdbbadebd282192136dcf051383e1d064f1ec
Groinkb <benjamin@mossu.ch>
2023-08-02 20:31:39 +0200

    NEW NODE END

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -32,15 +32,15 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (str == NULL)
 		return NULL;
 
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
 	if (new_node == NULL)
-		return (NULL);
+		return NULL;
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return (NULL);
+		return NULL;
 	}
 
 	new_node->len = _strlen(str);
@@ -59,5 +59,5 @@ list_t *add_node_end(list_t **head, const char *str)
 		last->next = new_node;
 	}
 
-	return (new_node);
+	return new_node;
 }
