a29a90074e751a566396e78fef9c6872a8575c1f
Groinkb <benjamin@mossu.ch>
2023-08-02 20:05:03 +0200

    Add new directory and print linked lsit

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list - prints all elements in list_t list
+ *
+ * @h: pointer to the start of the list
+ *
+ * Return: number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	if (h == NULL)
+		return 0;
+
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+	else
+	{
+		printf("[%d] %s\n", h->len, h->str);
+	}
+
+	return 1 + print_list(h->next);
+}
