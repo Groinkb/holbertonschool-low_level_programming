d46c4e2e07ad06640b68ec55854e2f4c99292677
Groinkb <benjamin@mossu.ch>
2023-08-02 20:13:26 +0200

    list lenght

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * list_len - Returns the number of elements in a linked list_t list
+ *
+ * @h: pointer to the start of the list
+ *
+ * Return: number of nodes in the list
+ */
+size_t list_len(const list_t *h)
+{
+	if (h == NULL)
+		return 0;
+
+	return 1 + list_len(h->next);
+}
