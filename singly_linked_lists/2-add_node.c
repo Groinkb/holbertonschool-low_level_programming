6947ec436770bb832020efb3377a1b4f7a472102
Groinkb <benjamin@mossu.ch>
2023-08-02 20:20:11 +0200

    node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,20 +13,20 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	if (str == NULL)
-		return (NULL);
+		return NULL;
 
 	list_t *new_node;
 	size_t len = strlen(str);
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-		return (NULL);
+		return NULL;
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return (NULL);
+		return NULL;
 	}
 
 	new_node->len = len;
@@ -34,5 +34,5 @@ list_t *add_node(list_t **head, const char *str)
 
 	*head = new_node;
 
-	return (new_node);
+	return new_node;
 }
