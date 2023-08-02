73ee82fe48238dd3d2d880d3196d535089755a87
Groinkb <benjamin@mossu.ch>
2023-08-02 21:11:23 +0200

    print distinct

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t co = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		printf("%d\n", h->n);
+		co++;
+	}
+
+	return co;
+}
