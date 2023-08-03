cd260c41ec31b48843bc7a0859f8e69f1df1cc22
Groinkb <you@example.com>
2023-08-03 16:41:53 +0200

    TEST COMMIT

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -5,7 +5,7 @@
 
 /**
  * dlistint_len - function that returns the number of elements in a,
- * linked dlistint_t list.
+ * linked dlistint_t list. TEST
  * @h: pointer to the head of the listd
  *
  * Return: number of elements
