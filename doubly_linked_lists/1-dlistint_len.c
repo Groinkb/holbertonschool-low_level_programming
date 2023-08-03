166cb02d137d8cf9ba60a0343767ece67ec0ec6e
Groinkb <you@example.com>
2023-08-03 16:40:36 +0200

    change comment

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -6,7 +6,7 @@
 /**
  * dlistint_len - function that returns the number of elements in a,
  * linked dlistint_t list.
- * @h: pointer to the head of the list
+ * @h: pointer to the head of the listd
  *
  * Return: number of elements
  */
