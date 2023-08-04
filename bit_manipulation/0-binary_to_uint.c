13030d78d49dc5b34a18f79e45f3306dfb7221ce
Groinkb <benjamin@mossu.ch>
2023-08-04 22:45:09 +0200

    désolé j'ai essayé

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,43 +1,27 @@
 #include "main.h"
 
 /**
- * binary_to_uint_recursive - converts a binary number
- * to unsigned int (helper function)
- * @b: string containing the binary number
- * @idx: current index in the string
- * @result: the partial result
- *
- * Return: 1 if successful, 0 otherwises
- */
-int (binary_to_uint_recursive)(const char *b, int idx, unsigned int *result)
-{
-	if (b[idx] == '\0')
-		return (1);
-
-	if (b[idx] == '0' || b[idx] == '1')
-	{
-		*result = (*result << 1) | (b[idx] - '0');
-		return binary_to_uint_recursive(b, idx + 1, result);
-	}
-
-	return (0);
-}
-
-/**
  * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
  *
  * Return: the converted number
  */
+
 unsigned int binary_to_uint(const char *b)
+
 {
+	int i;
 	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
 
-	if (binary_to_uint_recursive(b, 0, &dec_val))
-		return (dec_val);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
 
-	return (0);
+	return (dec_val);
 }
