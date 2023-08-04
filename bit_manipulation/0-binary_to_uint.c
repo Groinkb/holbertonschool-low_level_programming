4274f4273886e6dc3ca902104d386a3f50abd779
Groinkb <benjamin@mossu.ch>
2023-08-04 22:37:13 +0200

    Parenthese

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * binary_to_uint_recursive - converts a binary number to unsigned int (helper function)
+ * binary_to_uint_recursive - converts a binary number
+ * to unsigned int (helper function)
  * @b: string containing the binary number
  * @idx: current index in the string
  * @result: the partial result
@@ -11,7 +12,7 @@
 int binary_to_uint_recursive(const char *b, int idx, unsigned int *result)
 {
 	if (b[idx] == '\0')
-		return 1;
+		return (1);
 
 	if (b[idx] == '0' || b[idx] == '1')
 	{
@@ -19,7 +20,7 @@ int binary_to_uint_recursive(const char *b, int idx, unsigned int *result)
 		return binary_to_uint_recursive(b, idx + 1, result);
 	}
 
-	return 0;
+	return (0);
 }
 
 /**
@@ -33,10 +34,10 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int dec_val = 0;
 
 	if (!b)
-		return 0;
+		return (0);
 
 	if (binary_to_uint_recursive(b, 0, &dec_val))
-		return dec_val;
+		return (dec_val);
 
-	return 0;
+	return (0);
 }
