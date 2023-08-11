460f235036b1656e50953498fd47fb965ea4e68c
Groinkb <benjamin@mossu.ch>
2023-08-11 20:31:59 +0200

    Cobra

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -9,49 +9,47 @@
  * Return: Upon failure - 0.
  *         Otherwise - 1.
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+
 {
-	unsigned long int index;
-	hash_node_t *current, *new_node;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
 
-	for (current = ht->array[index]; current != NULL; current = current->next)
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
-		if (strcmp(current->key, key) == 0)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			free(current->value);
-			current->value = strdup(value);
-			if (current->value == NULL)
-				return (0);
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
 			return (1);
 		}
 	}
 
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
-		free(new_node);
+		free(value_copy);
 		return (0);
 	}
-
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
+	new->key = strdup(key);
+	if (new->key == NULL)
 	{
-		free(new_node->key);
-		free(new_node);
+		free(new);
 		return (0);
 	}
-
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
 	return (1);
 }
