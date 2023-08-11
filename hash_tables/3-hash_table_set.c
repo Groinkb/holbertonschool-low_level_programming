fe8e1c4b591909cd7a9bfacbc0cc1ef9c00267f2
Groinkb <benjamin@mossu.ch>
2023-08-11 20:30:48 +0200

    change

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,12 +11,11 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index;
+	hash_node_t *current, *new_node;
+
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-
-	unsigned long int index;
-	hash_node_t *current;
-	hash_node_t *new_node;
 
 	index = key_index((const unsigned char *)key, ht->size);
 
