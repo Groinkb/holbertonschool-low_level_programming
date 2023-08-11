63b2c78288c071488a36412702d0acdcb84e4cdc
Groinkb <benjamin@mossu.ch>
2023-08-11 20:40:02 +0200

    iso90 fixed

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -11,15 +11,18 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index;
+	hash_node_t *node;
+
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
 	if (index >= ht->size)
 		return (NULL);
 
-	hash_node_t *node = ht->array[index];
+	node = ht->array[index];
 	while (node != NULL && strcmp(node->key, key) != 0)
 	{
 		node = node->next;
