0c61c87e6c68bad09c2b879123b5e626745e1012
Groinkb <benjamin@mossu.ch>
2023-08-11 12:13:13 +0200

    3hahtables

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,54 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+
+	for (hash_node_t *current = ht->array[index]; current != NULL; current = current->next)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (current->value == NULL)
+				return (0);
+			return (1);
+		}
+	}
+
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
