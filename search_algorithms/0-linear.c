9d9bef9728375342e101afe10f95155677c6bb64
Groinkb <benjamin@mossu.ch>
2023-09-22 10:52:35 +0200

    fix linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -13,7 +13,8 @@ int linear_search(int *array, size_t size, int value)
 	if (array == NULL || size < 1)
 		return (-1);
 
-	for (size_t i = 0; i < size; i++)
+	size_t i;
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
