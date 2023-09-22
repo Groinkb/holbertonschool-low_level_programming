89af0694bd66e137139439c90aaf36b79cd0461f
Groinkb <benjamin@mossu.ch>
2023-09-22 10:51:12 +0200

    completed 0-linear.c

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Recherche un élément dans un tableau avec la méthode linéaire.
+ * @array: Pointeur vers le premier élément du tableau à rechercher.
+ * @size: Nombre d'éléments dans le tableau.
+ * @value: Valeur à rechercher.
+ *
+ * Return: Index de la première occurrence de la valeur, sinon -1.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	if (array == NULL || size < 1)
+		return (-1);
+
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
