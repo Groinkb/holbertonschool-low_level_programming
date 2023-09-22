e7a1b7886458f6d6969bcfb4278213c168796460
Groinkb <benjamin@mossu.ch>
2023-09-22 11:00:42 +0200

    fixed

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -10,19 +10,20 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
+	size_t low, high, mid, i;
+
 	if (array == NULL || size == 0)
 		return (-1);
 
-	size_t low = 0;
-	size_t high = size - 1;
-	size_t mid;
+	low = 0;
+	high = size - 1;
 
 	while (low <= high)
 	{
 		mid = (low + high) / 2;
 
 		printf("Searching in array: ");
-		for (size_t i = low; i <= high; i++)
+		for (i = low; i <= high; i++)
 		{
 			printf("%d", array[i]);
 			if (i < high)
